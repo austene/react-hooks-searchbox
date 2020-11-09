2a6998722f1b92a28be9b04ef4bbd5479efc374d
AE <austence@sbcglobal.net>
2020-11-09 09:19:56 -0800

    updates babel config and adds scaffold for searchbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <label for='searchbox'>Search</label>
+        <input
+          type='text'
+          id='searchbox'
+          name='searchbox'
+          value=''
+          placeholder='Enter Phrase'
+        />
+      </form>
     </div>
   );
 }
